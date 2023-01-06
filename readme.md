89f48d8af583368300e759b972b1c03390c37738
suryasai <suryasaichanti@gmail.com>
2023-01-06 14:48:03 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Tasks Manager
+
+A simple todo app that allows you to add, view, and delete todo items and their sub tasks.
+
+## Features
+
+- Add a todo item with a title and multiple sub tasks
+- View the todo list
+- Mark sub tasks as completed
+- Delete todo items and sub tasks
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Local Storage
+
+## How to Use
+
+1. Enter a todo title and sub tasks (separated by a comma) in the form and click "Add Todo"
+2. The todo and its sub tasks will be added to the todo list
+3. Check the checkbox next to a sub task to mark it as completed
+4. Click the trash icon next to a todo or sub task to delete it
+
+![outputUi](./Tasks%20Manager1.PNG)
---
+++ script.js
@@ -0,0 +1,102 @@
+const todoForm = document.querySelector('#todo-form');
+const todoList = document.querySelector('#todo-list');
+
+todoForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const todoTitle = todoForm.querySelector('#todo-title').value;
+  const todoSubTasks = todoForm.querySelector('#todo-sub-tasks').value;
+  const todo = {
+    title: todoTitle,
+    subTasks: todoSubTasks.split(',')
+  };
+  saveTodo(todo);
+  todoForm.reset();
+});
+
+function saveTodo(todo) {
+  let todos = getTodos();
+  todos.push(todo);
+  localStorage.setItem('taskManager', JSON.stringify(todos));
+  renderTodos();
+}
+
+
+function getTodos() {
+  let todos = localStorage.getItem('taskManager');
+  if (todos === null) {
+    return [];
+  }
+  return JSON.parse(todos);
+}
+
+function renderTodos() {
+  const todos = getTodos();
+  todoList.innerHTML = '';
+  todos.forEach((todo, todoIndex) => {
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo'); 
+    const todoHeadDiv = document.createElement('div');
+    const todoTitle = document.createElement('h2');
+    todoTitle.textContent = todo.title[0].toUpperCase() + todo.title.slice(1);
+    todoHeadDiv.appendChild(todoTitle); 
+    const deleteButton = document.createElement('i');
+    deleteButton.classList.add("fa-solid", "fa-trash");
+    deleteButton.addEventListener('click', () => {
+      deleteTodo(todoIndex);
+    });
+    
+    todoHeadDiv.appendChild(deleteButton);
+    todoDiv.appendChild(todoHeadDiv);
+    const todoSubTasks = document.createElement('ul');
+    todo.subTasks.forEach((subTask, subTaskIndex) => {
+      const li = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      const label = document.createElement('label');
+      label.textContent = subTask;
+      checkbox.addEventListener('change', (event) => {
+        if (event.target.checked) {
+          label.style.textDecoration = 'line-through';
+        } else {
+          label.style.textDecoration = 'none';
+        }
+      });
+      li.appendChild(checkbox);
+      li.appendChild(label);
+      const deleteSubTaskButton = document.createElement('i');
+      deleteSubTaskButton.classList.add("fa-solid" , "fa-trash");   
+      deleteSubTaskButton.addEventListener('click', () => {
+        deleteSubTask(todoIndex, subTaskIndex);
+      });
+      li.appendChild(deleteSubTaskButton);
+
+      todoSubTasks.appendChild(li);
+    });
+    todoDiv.appendChild(todoSubTasks);
+    todoList.appendChild(todoDiv);
+  });
+}
+
+
+function deleteTodo(index) { 
+  console.log("object");
+  let todos = getTodos();
+  todos.splice(index, 1);
+  localStorage.setItem('taskManager', JSON.stringify(todos));
+  renderTodos();
+}
+
+
+function deleteSubTask(todoIndex, subTaskIndex) {
+  let todos = getTodos();
+  todos[todoIndex].subTasks.splice(subTaskIndex, 1);
+  localStorage.setItem('taskManager', JSON.stringify(todos));
+  renderTodos();
+}
+
+
+renderTodos();
+
+  
+
+  
